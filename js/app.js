1f2d9f0de4ce5acdcb888054ad04399aba179d62
紀野　友宏 <rkanswer1994@gmail.com>
2013-12-12 15:14:11 +0900

    angular tuto ned

---
+++ js/app.js
@@ -0,0 +1,38 @@
+/* global angular: true, window: true */
+
+//var phonecatApp = angular.module('phonecatApp', ['ngRoute', 'phonecatControllers']);
+//
+//phonecatApp.config(['$routeProvider', function($routeProvider) {
+//	$routeProvider
+//	.when('/phones', {
+//		templateUrl: 'partials/phone-list.html',
+//		controller: 'PhoneListCtrl'
+//	})
+//	.when('/phones/:phoneId', {
+//		templateUrl: 'partials/phone-detail.html',
+//		controller: 'PhoneDetailCtrl'
+//	})
+//	.otherwise({
+//		redirectTo: '/phones'
+//	});
+//}]);
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatControllers'
+]);
+ 
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.hrml',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
--- js/controller.js
+++ js/controller.js
@@ -1,37 +1,31 @@
-/* global angular: true */
+/* global angular: true, window: true */
 
-var phonecatApp = angular.module('phonecatApp', []);	//モジュール名の指定
-
-//コントローラーの作成
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function ($scope, $http) {
-	$http({
-		method: 'GET',
-		url: './json/phones.json'
-	})
-	.success(function (data) {
-		$scope.phones = data;
-	})
-	.error(function (data, status, headers, config) {
-		console.log(data);
-		console.log(status);
-		console.log(headers);
-		console.log(config);
-	});
-	//jsonの作成
-//	$scope.phones = [
-//		{
-//			name: 'Nexus S',
-//			snippet: 'Fast just got faster with Nexus S.'
-//		},
-//		{
-//			name: 'Motorola XOOM™ with Wi-Fi',
-//			snippet: 'The Next, Next Generation tablet.'
-//		},
-//		{
-//			name: 'MOTOROLA XOOM™',
-//			snippet: 'The Next, Next Generation tablet.'
-//		}
-//	];
-	
-	$scope.orderProp = 'age';
-}]);
+//var phonecatControllers = angular.module('phonecatCotrollers', []);	//モジュール名の指定
+//
+////コントローラーの作成
+//phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http', function ($scope, $http) {
+//	$http.get('./json/phones.json')
+//	.success(function (data) {
+//		$scope.phones = data;
+//	});
+//	$scope.orderProp = 'age';
+//}]);
+//
+//phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+//	$scope.phoneId = $routeParams.phoneId;
+//}]);
+var phonecatControllers = angular.module('phonecatControllers', []);
+ 
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('json/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+ 
+    $scope.orderProp = 'age';
+  }]);
+ 
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+  }]);
